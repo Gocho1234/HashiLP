8c37816ea8a027ae41e1d27bc05e8ee92be7d229
Gocho1234 <goncalo.barias@gmail.com>
2022-01-11 21:04:08 +0000

    resolvi um bug no predicado 1

--- projeto.pl
+++ projeto.pl
@@ -1,14 +1,20 @@
 :- [codigo_comum, puzzles_publicos].
 
+%%% Predicados auxiliares
+
+remove_duplicados([], []).
+remove_duplicados([H|T], [H|T1]) :-
+  subtract(T, [H], T2), remove_duplicados(T2, T1).
+
+%%% Predicados oficiais
+
 extrai_ilhas_linha(N_linha, Linha, Ilhas) :-
   findall(ilha(El, (N_linha, N_coluna)),
           (member(El, Linha), El \== 0,
-          nth1(N_coluna, Linha, El)),
-          Ilhas).
+          nth1(N_coluna, Linha, El)), Ilhas_aux),
+  remove_duplicados(Ilhas_aux, Ilhas).
 
 ilhas(Puz, Ilhas) :-
-  findall(Ilha,
-          (member(Linha, Puz), nth1(N_linha, Puz, Linha),
-          extrai_ilhas_linha(N_linha, Linha, Ilha)),
-          Ilhas_aux),
+  findall(Ilha, (member(Linha, Puz), nth1(N_linha, Puz, Linha),
+          extrai_ilhas_linha(N_linha, Linha, Ilha)), Ilhas_aux),
   flatten(Ilhas_aux, Ilhas).
