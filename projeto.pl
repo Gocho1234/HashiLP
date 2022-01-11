bd58ad3e77cf075baa12592467df95f0fcea4874
Gocho1234 <goncalo.barias@gmail.com>
2022-01-11 20:44:04 +0000

    predicado 2

--- projeto.pl
+++ projeto.pl
@@ -1,10 +1,14 @@
-extrai_ilhas_linha(N_L, Linha, Ilhas) :-
-  extrai_ilhas_linha_aux(N_L, Linha, Linha, Ilhas), !.
-extrai_ilhas_linha_aux(_, [], _, []).
-extrai_ilhas_linha_aux(N_L, [El_1 | Resto_l], Linha, [Ilha_1 | Resto_i]) :-
-  El_1 \== 0,
-  nth1(N, Linha, El_1),
-  Ilha_1 = ilha(El_1, (N_L, N)),
-  extrai_ilhas_linha_aux(N_L, Resto_l, Linha, Resto_i).
-extrai_ilhas_linha_aux(N_L, [0 | Resto_l], Linha, Resto_i) :-
-  extrai_ilhas_linha_aux(N_L, Resto_l, Linha, Resto_i).
+:- [codigo_comum, puzzles_publicos].
+
+extrai_ilhas_linha(N_linha, Linha, Ilhas) :-
+  findall(ilha(El, (N_linha, N_coluna)),
+          (member(El, Linha), El \== 0,
+          nth1(N_coluna, Linha, El)),
+          Ilhas).
+
+ilhas(Puz, Ilhas) :-
+  findall(Ilha,
+          (member(Linha, Puz), nth1(N_linha, Puz, Linha),
+          extrai_ilhas_linha(N_linha, Linha, Ilha)),
+          Ilhas_aux),
+  flatten(Ilhas_aux, Ilhas).
