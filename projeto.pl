c627f465276c363e8c93d0606bdfa6a5db204454
Gocho1234 <goncalo.barias@gmail.com>
2022-01-12 15:14:16 +0000

    resolvi alguns bugs

--- projeto.pl
+++ projeto.pl
@@ -2,19 +2,21 @@
 
 %%% Predicados auxiliares
 
-remove_duplicados([], []).
-remove_duplicados([H|T], [H|T1]) :-
-  subtract(T, [H], T2), remove_duplicados(T2, T1).
+formata_ilhas(((N_lin, N_col), Ponte), ilha(Ponte, (N_lin, N_col))).
 
 %%% Predicados oficiais
 
-extrai_ilhas_linha(N_linha, Linha, Ilhas) :-
-  findall(ilha(El, (N_linha, N_coluna)),
-          (member(El, Linha), El \== 0,
-          nth1(N_coluna, Linha, El)), Ilhas_aux),
-  remove_duplicados(Ilhas_aux, Ilhas).
+extrai_ilhas_linha(N_lin, Lin, Ilhas) :-
+  setof(
+    ((N_lin, N_col), El), (member(El, Lin), El \== 0,
+    nth1(N_col, Lin, El)), Ilhas_aux
+  ),
+  maplist(formata_ilhas, Ilhas_aux, Ilhas).
+
 
 ilhas(Puz, Ilhas) :-
-  findall(Ilha, (member(Linha, Puz), nth1(N_linha, Puz, Linha),
-          extrai_ilhas_linha(N_linha, Linha, Ilha)), Ilhas_aux),
+  findall(
+    Ilha, (member(Lin, Puz), nth1(N_lin, Puz, Lin),
+    extrai_ilhas_linha(N_lin, Lin, Ilha)), Ilhas_aux
+  ),
   flatten(Ilhas_aux, Ilhas).
