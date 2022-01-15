8c1c1ad2e523aa8eb457cf59bcc22b76185d4416
Gocho1234 <goncalo.barias@gmail.com>
2022-01-15 20:33:28 +0000

    1600 acceptedvim projeto.plvim projeto.pl

--- projeto.pl
+++ projeto.pl
@@ -10,16 +10,14 @@
 %                                                                      %
 % -------------------------------------------------------------------- %
 
-:- [codigo_comum].
-
 % ------------------------------------------------------------------------------
 % extrai_ilhas_linha(N_lin, Lin, Ilhas)
 % ------------------------------------------------------------------------------
 
 extrai_ilhas_linha(N_lin, Lin, Ilhas) :-
   setof(
-    ilha(Ponte, (N_lin, N_col)), (nth1(N_col, Lin, Ponte),
-    Ponte \== 0), Ilhas_aux
+    ilha(N_Pontes, (N_lin, N_col)), (nth1(N_col, Lin, N_Pontes),
+    N_Pontes \== 0), Ilhas_aux
   ),
   sort(2, @=<, Ilhas_aux, Ilhas); Ilhas = [].
 
@@ -35,6 +33,7 @@ ilhas(Puz, Ilhas) :-
 
 % ------------------------------------------------------------------------------
 % adjacente(Adj, El0, El1)
+% Predicado auxiliar
 % ------------------------------------------------------------------------------
 
 adjacente(Adj, El0, El1) :-
@@ -42,10 +41,10 @@ adjacente(Adj, El0, El1) :-
   append(_, [El1,El0|_], Adj).
 
 % ------------------------------------------------------------------------------
-% vizinhas(Ilhas, Ilha, Viz)
+% vizinhas(Ilhas, Ilha, Vz)
 % ------------------------------------------------------------------------------
 
-vizinhas(Ilhas, Ilha, Viz) :-
+vizinhas(Ilhas, Ilha, Vz) :-
   ilha(_, (N_l, N_c)) = Ilha,
   findall(
     Ilha_viz, (member(Ilha_viz, Ilhas),
@@ -55,8 +54,8 @@ vizinhas(Ilhas, Ilha, Viz) :-
     Ilha_viz, (member(Ilha_viz, Ilhas),
     Ilha_viz = ilha(_, (_, N_C)), N_C == N_c), Col_aux
   ), include(adjacente(Col_aux, Ilha), Col_aux, Col),
-  append(Lin, Col, Viz_aux),
-  sort(2, @=<, Viz_aux, Viz).
+  append(Lin, Col, Vz_aux),
+  sort(2, @=<, Vz_aux, Vz).
 
 % ------------------------------------------------------------------------------
 % estado(Ilhas, Estado)
@@ -70,6 +69,7 @@ estado(Ilhas, Estado) :-
 
 % ------------------------------------------------------------------------------
 % entre(Pos1, Pos2, Posicoes)
+% Predicado auxiliar
 % ------------------------------------------------------------------------------
 
 entre(Pos1, Pos2, Posicoes) :-
@@ -101,20 +101,117 @@ cria_ponte(Pos1, Pos2, Ponte) :-
 % caminho_livre(Pos1, Pos2, Posicoes, I, Vz)
 % ------------------------------------------------------------------------------
 
-caminho_livre(_, _, Posicoes, ilha(_, PosI), ilha(_, PosVz)) :-
+caminho_livre(Pos1, Pos2, Posicoes, ilha(_, PosI), ilha(_, PosVz)) :-
   posicoes_entre(PosI, PosVz, PosEntre),
   findall(
     Pos, (member(Pos, PosEntre), subset([Pos], Posicoes)),
     Posicoes_comum
-  ), length(Posicoes_comum, Len), Len \== 1.
+  ), length(Posicoes_comum, Len),
+  (Len \== 1 ; lists:perm([Pos1, Pos2], [PosI, PosVz])).
 
 % ------------------------------------------------------------------------------
 % actualiza_vizinhas_entrada(Pos1, Pos2, Posicoes, Entrada, Nova_Entrada)
 % ------------------------------------------------------------------------------
 
-actualiza_vizinhas_entrada(_, _, Posicoes, [I, Vz, []], [I, Vz_novo, []]) :-
-  findall(
+actualiza_vizinhas_entrada(Pos1, Pos2, Posicoes, [I, Vz, Pontes], [I, Vz_novo, Pontes]) :-
+  setof(
     Pos, (member(Pos, Vz),
-    caminho_livre(_, _, Posicoes, I, Pos)),
-    Vz_novo
+    caminho_livre(Pos1, Pos2, Posicoes, I, Pos)),
+    Vz_aux
+  ),
+  sort(2, @=<, Vz_aux, Vz_novo), !; Vz_novo = [].
+
+% ------------------------------------------------------------------------------
+% actualiza_vizinhas_apos_pontes(Estado, Pos1, Pos2, Novo_estado)
+% ------------------------------------------------------------------------------
+
+actualiza_vizinhas_apos_pontes(Estado, Pos1, Pos2, Novo_estado) :-
+  posicoes_entre(Pos1, Pos2, Posicoes),
+  findall(
+    Nova_Entrada, (member(Entrada, Estado),
+    actualiza_vizinhas_entrada(Pos1, Pos2, Posicoes, Entrada, Nova_Entrada)),
+    Novo_estado
   ).
+
+% ------------------------------------------------------------------------------
+% ilhas_terminadas(Estado, Ilhas_term)
+% ------------------------------------------------------------------------------
+
+ilhas_terminadas(Estado, Ilhas_term) :-
+  findall(
+    Ilha, (member([Ilha, _, Pontes], Estado), Ilha = ilha(N_Pontes, _),
+    N_Pontes \== 'X', length(Pontes, N_Pontes)), Ilhas_term
+  ).
+
+% ------------------------------------------------------------------------------
+% tira_ilhas_terminadas_entrada(Ilhas_term, Entrada, Nova_Entrada)
+% ------------------------------------------------------------------------------
+
+tira_ilhas_terminadas_entrada(Ilhas_term, [I, Vz, Pontes], [I, Vz_novo, Pontes]) :-
+  subtract(Vz, Ilhas_term, Vz_novo).
+
+% ------------------------------------------------------------------------------
+% tira_ilhas_terminadas(Estado, Ilhas_term, Novo_estado)
+% ------------------------------------------------------------------------------
+
+tira_ilhas_terminadas(Estado, Ilhas_term, Novo_estado) :-
+  maplist(
+    tira_ilhas_terminadas_entrada(Ilhas_term),
+    Estado, Novo_estado
+  ).
+
+% ------------------------------------------------------------------------------
+% marca_ilhas_terminadas_entrada(Ilhas_term, Entrada, Nova_Entrada)
+% ------------------------------------------------------------------------------
+
+marca_ilhas_terminadas_entrada(Ilhas_term, [Ilha, Vz, Pontes], [Nova_Ilha, Vz, Pontes]) :-
+  Ilha = ilha(_, (N_lin, N_col)),
+  (subset([Ilha], Ilhas_term), Nova_Ilha = ilha('X', (N_lin, N_col)) ;
+  Nova_Ilha = Ilha).
+
+% ------------------------------------------------------------------------------
+% marca_ilhas_terminadas(Estado, Ilhas_term, Novo_estado)
+% ------------------------------------------------------------------------------
+
+marca_ilhas_terminadas(Estado, Ilhas_term, Novo_estado) :-
+  maplist(
+    marca_ilhas_terminadas_entrada(Ilhas_term),
+    Estado, Novo_estado
+  ).
+
+% ------------------------------------------------------------------------------
+% trata_ilhas_terminadas(Estado, Novo_estado)
+% ------------------------------------------------------------------------------
+
+trata_ilhas_terminadas(Estado, Novo_estado) :-
+  ilhas_terminadas(Estado, Ilhas_term),
+  tira_ilhas_terminadas(Estado, Ilhas_term, Estado_aux),
+  marca_ilhas_terminadas(Estado_aux, Ilhas_term, Novo_estado).
+
+% ------------------------------------------------------------------------------
+% adiciona_pontes(Num_pontes, Ilha1, Ilha2, Entrada, Nova_Entrada)
+% ------------------------------------------------------------------------------
+
+adiciona_pontes(Num_pontes, Ilha1, Ilha2, Entrada, Nova_Entrada) :-
+  Ilha1 = ilha(_, Pos1), Ilha2 = ilha(_, Pos2),
+  Entrada = [Ilha, Vzs, _],
+  cria_ponte(Pos1, Pos2, Ponte_aux),
+  (Num_pontes == 1, Ponte = [Ponte_aux] ;
+  Num_pontes == 2, append([Ponte_aux], [Ponte_aux], Ponte)),
+  (Ilha == Ilha1, append([], Ponte, Pontes) ;
+  Ilha == Ilha2, append([], Ponte, Pontes) ;
+  Pontes = []),
+  Nova_Entrada = [Ilha, Vzs, Pontes].
+
+% ------------------------------------------------------------------------------
+% junta_pontes(Estado, Num_pontes, Ilha1, Ilha2, Novo_estado)
+% ------------------------------------------------------------------------------
+
+junta_pontes(Estado, Num_pontes, Ilha1, Ilha2, Novo_estado) :-
+  Ilha1 = ilha(_, Pos1), Ilha2 = ilha(_, Pos2),
+  maplist(
+    adiciona_pontes(Num_pontes, Ilha1, Ilha2),
+    Estado, Estado_aux
+  ), !,
+  actualiza_vizinhas_apos_pontes(Estado_aux, Pos1, Pos2, Novo_estado_aux),
+  trata_ilhas_terminadas(Novo_estado_aux, Novo_estado).
